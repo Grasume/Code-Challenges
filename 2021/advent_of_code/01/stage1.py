48fe7c0d46e96e0d5ed1779e3a21075c36fcb1bc
grasume <jamesmgerstenberg@gmail.com>
2021-12-03 14:42:50 -0600

    Day 1 completed

--- 2021/advent_of_code/01/stage1.py
+++ 2021/advent_of_code/01/stage1.py
@@ -1,52 +1,42 @@
 
+def question_1_part_1():
+    with  open("input.txt", "r") as text_file:
+        depths = text_file.read().splitlines()
+        depth_increase=0
+        depth_decrease=0
 
-with  open("input.txt", "r") as text_file:
-    depths = text_file.read().splitlines()
+        for depth_value in range(len(depths) - 1):
+            if int(depths[depth_value]) < int(depths[depth_value + 1]):
+                depth_increase += 1
+            else:
+                depth_decrease += 1
+    print("Part 1")
+    print("Total Times Depth Increased ", depth_increase)
+    print("Total Times Depth Decreased ", depth_decrease)
 
-    depth_increase=0
-    depth_decrease=0
+def question_1_part_2():
+    with  open("input.txt", "r") as text_file:
+        depths = text_file.read().splitlines()
+        depth_increase=0
+        depth_decrease=0
 
-    for depth_value in range(len(depths) - 1):
-        if int(depths[depth_value]) < int(depths[depth_value + 1]):
-            depth_increase += 1
-            #print("Increasing")
-        else:
-            depth_decrease += 1
-            #print("Decreasing")
-
-print("Total Times Depth Increased ", depth_increase)
-print("Total Times Depth Decreased ", depth_decrease)
-
-with open("input.txt", "r") as f:
-    depths = f.read().splitlines()
-
-count = 0
-for i in range(len(depths) - 1):
-    if int(depths[i]) < int(depths[i + 1]):
-        count += 1
-print(count)
-
-with open("input.txt", "r") as f:
-    depths = f.readlines()
-
-count = 0
-for i, num in enumerate(depths[:-1]):
-    if int(num) < int(depths[i + 1]):
-        count += 1
-
-print(count)
+        for depth_value in range(len(depths) - 3):
+            if int(depths[depth_value]) < int(depths[depth_value + 3]):
+                depth_increase += 1
+            else:
+                depth_decrease += 1
+    print("Part 2")
+    print("Total Times Depth Increased ", depth_increase)
+    print("Total Times Depth Decreased ", depth_decrease)
 
 
-text_file = open("input.txt", "r")
-depth = text_file.readlines()
-#depth = map(lambda s: int(s.strip()), depth)
-text_file.close()
-depthlength=len(depth)
-increase=0
-decrease=0
-for index in range(1,depthlength):
-    if depth[index]>depth[index-1]:
-        increase=increase+1
-    elif depth[index]<depth[index-1]:
-        decrease=decrease+1
-print("Increases: {}\nDecreases: {}".format(increase,decrease))
+def question_1_simple():
+    data = [int(x) for x in open("input.txt", "r")]
+    print("Part 1:", sum([data[i] > data[i-1] for i in range(1, len(data))]))
+    print("Part 2:", sum([data[i] > data[i-3] for i in range(3, len(data))]))
+
+
+
+question_1_part_1()
+question_1_part_2()
+question_1_simple()
